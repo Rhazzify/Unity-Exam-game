1d3c4be7f55bef02e1d8742d35457a572f2c3e27
Kittikorn.D <Kittikorn-detnoi@hotmail.com>
2020-04-13 19:47:43 +0200

    Testing of UI

---
+++ Unity-Exam-Puzzle/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+   public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetScreenResFull()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    public void SetScreenResWindow()
+    {
+        Screen.SetResolution(1280, 720, false);
+    }
+
+
+}
