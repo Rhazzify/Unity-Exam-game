cfb6e0c53485d3be4c710b40b82c0f77ba2ea8df
Kaja Holte <56083298+kajaHolte@users.noreply.github.com>
2020-04-22 15:12:54 +0200

    ny

---
+++ Unity-Exam-Puzzle/Assets/Scripts/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public GameObject trigger;
+
+    void Start()
+    {
+        trigger.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            trigger.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            trigger.SetActive(false);
+        }
+    }
+}
